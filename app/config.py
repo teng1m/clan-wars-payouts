5e886edb7b35c1381bc8e815fef2ad0eed3b1c49
teng1m <katoryx@gmail.com>
2026-06-07 16:49:26 -0400

    strip trailing slash on base url

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 WG_APPLICATION_ID = os.getenv("WG_APPLICATION_ID", "")
-BASE_URL = os.getenv("BASE_URL", "http://localhost:8000")
+BASE_URL = os.getenv("BASE_URL", "http://localhost:8000").rstrip("/")
 SECRET_KEY = os.getenv("SECRET_KEY", "")
 
 _PROJECT_ROOT = Path(__file__).parent.parent
